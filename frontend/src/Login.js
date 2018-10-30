7487dc89ad66c0519f9e432f1bd5a15ae796010d
doctorpillcosby <gordontyrell1993@gmail.com>
2018-10-30 00:31:24 -0400

    adding semantic ui

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -5,7 +5,7 @@ class Login extends Component {
 
 login = (e) => {
   e.preventDefault()
-  
+
     this.props.setUsername(e.target.username.value)
 
 }
@@ -15,9 +15,11 @@ login = (e) => {
   render() {
     return (
       <div id="login">
-      <form onSubmit={this.login}>
+      <form class="ui form" onSubmit={this.login}>
         <label>Username:</label><br/>
+        <div class="field">
         <input type="text" id="username"/><br/>
+        </div>
         <input type="submit" value="Log In"/>
       </form>
 
